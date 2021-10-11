7ef77d9a5dc2e341445cfcce84f1ef2fb4bc93c7
Dennis Zhao <hozhao@ucsd.edu>
2021-10-10 17:44:10 -0700

    Yay! I finished the first lab!

--- hello_world2.cpp
+++ hello_world2.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[])
 {
 	std::ofstream ofs ("hello.txt", std::ofstream::out);
-	ofs << "Hello cse142L!\n";
+	ofs << "Hello hozhao@ucsd.edu!\n";
 	ofs.close();
 	return 0;  
 }
